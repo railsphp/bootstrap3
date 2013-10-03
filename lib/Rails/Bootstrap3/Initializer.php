27241edfdaab2fb0e75542fd39f6931b02afbe8e
Parziphal <blackmesa@ubuntu-server.com>
2013-10-02 21:00:09 -0500

    fixed file patterns

--- lib/Rails/Bootstrap3/Initializer.php
+++ lib/Rails/Bootstrap3/Initializer.php
@@ -8,10 +8,10 @@ class Initializer
         \Rails::assets()->addPaths([realpath(__DIR__ . '/../../vendor/assets/javascripts')]);
         \Rails::assets()->addPaths([realpath(__DIR__ . '/../../vendor/assets/stylesheets')]);
         \Rails::assets()->addFilePatterns([
-            '.eot',
-            '.svg',
-            '.ttf',
-            '.woff',
+            '*.eot',
+            '*.svg',
+            '*.ttf',
+            '*.woff',
         ]);
         \Rails\ActionView\ViewHelpers::addHelper('Rails\Bootstrap3\ViewHelper');
     }
